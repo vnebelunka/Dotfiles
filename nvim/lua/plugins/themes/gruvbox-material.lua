2284d40711eb3eff31ba3f4526e5bb962407f9c1
massia <massia@my.msu.ru>
2026-04-04 12:56:39 +0300

    switch to hard theme

--- nvim/lua/plugins/themes/gruvbox-material.lua
+++ nvim/lua/plugins/themes/gruvbox-material.lua
@@ -4,7 +4,7 @@ return {
     priority = 2000,
     config = function()
       -- Optional: Configure the palette/contrast before loading
-      vim.g.gruvbox_material_background = "soft" -- 'hard', 'medium', 'soft'
+      vim.g.gruvbox_material_background = "hard" -- 'hard', 'medium', 'soft'
       vim.g.gruvbox_material_foreground = "material" -- 'material', 'mix', 'original'
       vim.g.gruvbox_material_better_performance = 1
 
